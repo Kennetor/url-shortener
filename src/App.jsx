7c699e3e767a1bcf813372a60f05af352d75f6ad
kenneth <kenneth1.kodehode@gmail.com>
2023-01-28 18:38:31 +0100

    t

--- src/App.jsx
+++ src/App.jsx
@@ -8,6 +8,7 @@ import customizable from "./images/icon-fully-customizable.svg";
 import boost from "./images/bg-boost-mobile.svg";
 // Components
 import Footer from "./component/footer";
+// import MyComponent from "./component/api";
 
 function App() {
   return (
@@ -42,8 +43,8 @@ function App() {
             links are performing
           </p>
         </div>
-        <div className="btn absolute text-[18px] rounded-full left-[50%] -translate-x-[50%] mt-[17rem] pl-10 pr-10 bg-[#2acfcf] text-white border-none">
-          <button className="">Get started</button>
+        <div className="h-16 absolute text-[20px] rounded-full right-[50%] translate-x-[50%] mt-[17rem] pl-10 pr-10 bg-[#2acfcf] text-white border-none">
+          <button className="mt-4">Get started</button>
         </div>
       </div>
 
@@ -62,11 +63,12 @@ function App() {
               />
               {/* Gray background of html from here and down */}
 
-              <div className="five w-72 m-auto rounded-lg border-none py-[12px] px-20 bg-[#2acfcf] text-xl text-white mt-4">
+              <button className="five w-72 m-auto rounded-lg border-none py-[12px] px-20 bg-[#2acfcf] text-xl text-white mt-4">
                 Shorten it!
-              </div>
+              </button>
             </div>
           </div>
+          {/* Shortened Links here  */}
 
           <div>
             <h1 className="text-3xl">Advanced Statistics</h1>
